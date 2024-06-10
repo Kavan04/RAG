d288974de2bc707385d432ff2b8cc24a462b9c4f
Bold04 <94693357+Bold04@users.noreply.github.com>
2024-06-10 13:00:33 +0530

    fix: db syncing and file import

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask,request
+from flask import Flask, request
 from langchain_community.llms import Ollama
+from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_community.document_loaders import PDFPlumberLoader
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.prompts import PromptTemplate
+
 
 app = Flask(__name__)
+folder_path = "db"
 
 cached_llm = Ollama(model = "llama3")
-
+embedding = FastEmbedEmbeddings()
 
 
 @app.route('/ai', methods=['POST'])
@@ -20,18 +29,70 @@ def aiPost():
     response_answer = {"answer": response}
     return response_answer
 
+@app.route("/ask_pdf", methods=["POST"])
+def askPDFPost():
+    print("Post /ask_pdf called")
+    json_content = request.json
+    query = json_content.get("query")
 
-@app.route('/pdf', methods= ['POST'])
+    print(f"query: {query}")
+
+    print("Loading vector store")
+    vector_store = Chroma(persist_directory=folder_path, embedding_function=embedding)
+
+    print("Creating chain")
+    retriever = vector_store.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={
+            "k": 20,
+            "score_threshold": 0.1,
+        },
+    )
+
+    document_chain = create_stuff_documents_chain(cached_llm, raw_prompt)
+    chain = create_retrieval_chain(retriever, document_chain)
+
+    result = chain.invoke({"input": query})
+
+    print(result)
+
+    sources = []
+    for doc in result["context"]:
+        sources.append(
+            {"source": doc.metadata["source"], "page_content": doc.page_content}
+        )
+
+    response_answer = {"answer": result["answer"], "sources": sources}
+    return response_answer
+
+@app.route("/pdf", methods=["POST"])
 def pdfPost():
-    file = request.files["files"]
+    file = request.files["file"]
     file_name = file.filename
     save_file = "pdf/" + file_name
     file.save(save_file)
     print(f"filename: {file_name}")
 
-    response = {"status": "Successfully uploaded", "filename": file_name}
-    return response
+    loader = PDFPlumberLoader(save_file)
+    docs = loader.load_and_split()
+    print(f"docs len={len(docs)}")
 
+    chunks = text_splitter.split_documents(docs)
+    print(f"chunks len={len(chunks)}")
+
+    vector_store = Chroma.from_documents(
+        documents=chunks, embedding=embedding, persist_directory=folder_path
+    )
+
+    vector_store.persist()
+
+    response = {
+        "status": "Successfully Uploaded",
+        "filename": file_name,
+        "doc_len": len(docs),
+        "chunks": len(chunks),
+    }
+    return response
 
 def start_app():
     app.run( host ="0.0.0.0",port = 8080 , debug =True)
